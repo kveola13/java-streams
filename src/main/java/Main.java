1a2685b093849ba8fc28b647d35a35f78691b702
olakv <kveola13@student.westerdals.no>
2020-06-03 21:17:05 +0200

    added "normal" way of doing it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,31 @@
 package main.java;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = getPeople();
+
+        List<Person> females = new ArrayList<>();
+        for (Person person : people) {
+            if (person.getGender().equals(Gender.FEMALE)) {
+                females.add(person);
+            }
+        }
+        females.forEach(System.out::println);
+
     }
-    private static List<Person> getPeople(){
+
+    private static List<Person> getPeople() {
         return List.of(
                 new Person("Warwick", 12, Gender.MALE),
                 new Person("Lux", 21, Gender.FEMALE),
                 new Person("Kennen", 10, Gender.MALE),
                 new Person("Miss Fortune", 5, Gender.FEMALE),
-                new Person("Zac", 23, Gender.MALE)
+                new Person("Zac", 23, Gender.MALE),
+                new Person("Anivia", 100, Gender.FEMALE)
         );
     }
 }
