02477708cfa3aff77b46802db012aff0f73f3721
olakv <kveola13@student.westerdals.no>
2020-06-03 21:22:38 +0200

    some examples of streams

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,25 @@
 package main.java;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = getPeople();
-
         List<Person> females = new ArrayList<>();
         for (Person person : people) {
             if (person.getGender().equals(Gender.FEMALE)) {
                 females.add(person);
             }
         }
-        females.forEach(System.out::println);
+        //females.forEach(System.out::println);
         List<Person> femaleStream = people.stream().filter(person -> person.getGender().equals(Gender.FEMALE)).collect(Collectors.toList());
-        femaleStream.forEach(System.out::println);
+        //femaleStream.forEach(System.out::println);
+        //people.forEach(System.out::println);
+        List<Person> peopleSorted = people.stream().sorted(Comparator.comparing(Person::getAge)).collect(Collectors.toList());
+        peopleSorted.forEach(System.out::println);
     }
 
     private static List<Person> getPeople() {
