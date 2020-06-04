a668f8ffda733cdf8c62ed622a64b35889b7cb32
olakv <kveola13@student.westerdals.no>
2020-06-04 21:01:11 +0200

    added more list comprehension

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ package main.java;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Main {
